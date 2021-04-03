75d4580061a955e5da210c45e0813841ed69d148
hadilfriaa <hadil.friaa@ynov.com>
2021-04-03 15:34:25 +0200

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const userRouter = require('./users.route');
 const productRouter = require('./products.route');
 const orderRouter = require('./orders.route');
+const categoryRouter= require('./category.route');
+const checkoutRouter = require('./checkout.route');
 
 router.use(userRouter);
 router.use(productRouter);
 router.use(orderRouter);
+router.use(categoryRouter);
+router.use(checkoutRouter);
+
 
 module.exports = router;
